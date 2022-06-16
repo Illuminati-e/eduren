96ef6479d681590d3fdcd50cf2d4eb4e5bd92510
UjjvalShah <mrshah382531@gmail.com>
2022-06-17 03:53:22 +0530

    FAQ and Accordion

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import FAQ from './layouts/FAQ'
+import {Routes,Route}  from 'react-router-dom'
+
 import Home from './layouts/Home'
 const App = () => {
   return (
     <div>
-      <Home />
+      {/* <Home /> */}
+      <FAQ/>
     </div>
   )
 }
